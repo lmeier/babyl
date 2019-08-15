e8e5af8f84976fedd1642368157392b0ad33794f
Liam Meier <liammeier@gmail.com>
2019-08-14 22:40:34 -0700

    add static files

--- babylproject/settings.py
+++ babylproject/settings.py
@@ -15,8 +15,6 @@ from decouple import config, Csv
 import dj_database_url
 
 
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -204,6 +202,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
